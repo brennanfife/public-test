8e89e8d2e76a78a5f7398c21fbb82baeff441d9d
brennanfife <brenfife@gmail.com>
2023-07-05 17:51:41 -0600

    fix: description

--- src/pages/u/[username]/index.tsx
+++ src/pages/u/[username]/index.tsx
@@ -1,12 +1,16 @@
 import Head from "@/components/Head/Head";
 
-export default function PublicProfilePage({ profile }: { profile: { name: string; username: string } }) {
+export default function PublicProfilePage({
+  profile,
+}: {
+  profile: { name: string; username: string; activeProjects: number };
+}) {
   return (
     <>
       <Head
         title={`${profile.name} (${profile.username})`}
         type="profile"
-        description="View their Cantina profile"
+        description={`This user currently has ${profile.activeProjects} active projects.`}
         profile={{
           username: profile.username,
         }}
@@ -19,10 +23,11 @@ export default function PublicProfilePage({ profile }: { profile: { name: string
 export const getServerSideProps = async ({ params: { username } }: any) => {
   try {
     const name = "Brennan Fife";
+    const activeProjects = 5;
 
     return {
       props: {
-        profile: { name, username },
+        profile: { name, username, activeProjects },
       },
     };
   } catch (error) {
